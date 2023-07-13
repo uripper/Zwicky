c6a1fe7aabdc746ba1403353c573ba13aa48e8a9
uripper <kripper11@gmail.com>
2023-07-13 04:56:20 -0600

    The changes made in this commit include: - Added config.toml - Modified main.py to refactor the code into a class called Zwicky, which   contains methods for setting up the GUI, getting the transcript, cleaning   the transcript, computing word frequencies, and saving the frequencies to a   file. The class also reads from the config.toml file to set up the GUI and   get the locale and language codes. - Deleted the language_list variable from the get_information function.

--- main.py
+++ main.py
@@ -1,119 +1,109 @@
+# Refactored Code
+
 from youtube_transcript_api import YouTubeTranscriptApi
 from tkinter import *
 from tkinter import ttk
 import locale
+import tomllib
 
 
-def get_information():
-    global ENTRYFIELD, COMBOBOX
-    LANGCHOICE = COMBOBOX.get()
-    VIDEOID = ENTRYFIELD.get()
-    USER_FILENAME = FILENAME.get()
-    if LANGCHOICE == 'English':
-        locale.setlocale(locale.LC_ALL, 'en_EN.UTF-8')
-        local_lang = 'en'
-    elif LANGCHOICE == 'French':
-        locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')
-        local_lang = 'fr'
-    elif LANGCHOICE == 'Spanish':
-        locale.setlocale(locale.LC_ALL, 'es_ES.UTF-8')
-        local_lang = 'es'
-    elif LANGCHOICE == 'German':
-        locale.setlocale(locale.LC_ALL, 'de_DE.UTF-8')
-        local_lang = 'de'
-    elif LANGCHOICE == 'Italian':
-        locale.setlocale(locale.LC_ALL, 'it_IT.UTF-8')
-        local_lang = 'it'
-    elif LANGCHOICE == 'Japanese':
-        locale.setlocale(locale.LC_ALL, 'ja_JP.UTF-8')
-        local_lang = 'ja'
-    elif LANGCHOICE == 'Korean':
-        locale.setlocale(locale.LC_ALL, 'ko_KR.UTF-8')
-        local_lang = 'ko'
-    elif LANGCHOICE == 'Portuguese':
-        locale.setlocale(locale.LC_ALL, 'pt_PT.UTF-8')
-        local_lang = 'pt'
-    elif LANGCHOICE == 'Russian':
-        locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
-        local_lang = 'ru'
-    elif LANGCHOICE == 'Chinese':
-        locale.setlocale(locale.LC_ALL, 'zh_CN.UTF-8')
-        local_lang = 'zh'
-    elif LANGCHOICE == 'Turkish':
-        locale.setlocale(locale.LC_ALL, 'tr_TR.UTF-8')
-        local_lang = 'tr'
-    elif LANGCHOICE == 'Ukrainian':
-        locale.setlocale(locale.LC_ALL, 'uk_UA.UTF-8')
-        local_lang = 'uk'
-    elif LANGCHOICE == 'Vietnamese':
-        locale.setlocale(locale.LC_ALL, 'vi_VN.UTF-8')
-        local_lang = 'vi'
+class Zwicky:
+    def __init__(self):
+        with open("config.toml", "rb") as f:
+            self.config = tomllib.load(f)
 
-    subtitles = YouTubeTranscriptApi.get_transcript(
-        VIDEOID, languages=[local_lang])
-    usable_words = []
-    frequency = {}
-    subtitles_parsed = [i['text'] for i in subtitles]
-    cleaned_words = [
-        i.replace("\n", " ")
-        .replace(".", " ")
-        .replace("(", "")
-        .replace(")", "")
-        .replace("?", "")
-        .replace("!", "")
-        .replace(",", "")
-        .replace(";", "")
-        .replace(":", "")
-        .replace('"', '')
-        .replace("-", "")
-        for i in subtitles_parsed
-    ]
-    for i in cleaned_words:
-        words = i.split(" ")
-        usable_words.extend(i.lower() for i in words)
-    for i in usable_words:
-        if i == "":
-            continue
-        elif i in frequency:
-            frequency[i] += 1
-        else:
-            frequency[i] = 1
-    sorted_frequency = sorted(
-        frequency.items(), key=lambda x: x[1], reverse=True)
-    with open(f"{USER_FILENAME}.txt", "w", encoding='utf-8') as f:
-        for word, frequencies in sorted_frequency:
-            f.write(f"{word}: {str(frequencies)}" + "\n")
-    root.destroy()
+        self.locale_codes = self.config["locale_codes"]
+        self.language_codes = self.config["language_codes"]
 
+        self.root = Tk()
+        self.root.title("Word Frequency Analyzer")
+        self.font_general = ("Roboto", 14)
+        self.frm = ttk.Frame(self.root, padding=20)
+        self.frm.grid()
+        self.combobox = ttk.Combobox(
+            self.frm, values=self.config["language_list"], state="readonly"
+        )
+        self.entryfield = ttk.Entry(self.frm, width=10)
+        self.filename = ttk.Entry(self.frm, width=25)
+        self.setup_gui()
 
-language_list = ['English', 'French', 'Spanish', 'German', 'Italian',
-                 'Korean', 'Portuguese', 'Russian', 'Turkish', 'Ukrainian']
+    def setup_gui(self):
+        ttk.Label(self.frm, text="Enter Youtube Video ID").grid(
+            column=0, row=1, sticky=W
+        )
+        ttk.Label(self.frm, text="Target Language").grid(column=0, row=2, sticky=W)
+        self.combobox.grid(column=1, row=2, sticky=E)
+        self.entryfield.grid(column=1, row=1, sticky=E)
+        ttk.Label(self.frm, text="Enter Desired Filename").grid(
+            column=0, row=4, sticky=W
+        )
+        self.filename.grid(column=1, row=4, sticky=E)
+        ttk.Button(self.frm, text="Begin", command=self.get_information).grid(
+            column=0, row=5, sticky=W
+        )
+        ttk.Button(self.frm, text="Quit", command=self.root.destroy).grid(
+            column=1, row=5, sticky=E
+        )
 
-root = Tk()
-root.title("Word Frequency Analyzer")
-font_general = ('Roboto', 14)
-frm = ttk.Frame(root, padding=20)
-frm.grid()
+    def get_transcript(self, video_id, lang):
+        return YouTubeTranscriptApi.get_transcript(video_id, languages=[lang])
 
+    def clean_transcript(self, transcript):
+        cleaned_words = []
+        subtitles_parsed = [i["text"] for i in transcript]
+        for i in subtitles_parsed:
+            i = (
+                i.replace("\n", " ")
+                .replace(".", " ")
+                .replace("(", "")
+                .replace(")", "")
+                .replace("?", "")
+            )
+            i = (
+                i.replace("!", "")
+                .replace(",", "")
+                .replace(";", "")
+                .replace(":", "")
+                .replace('"', "")
+                .replace("-", "")
+            )
+            words = i.split(" ")
+            cleaned_words.extend(i.lower() for i in words)
+        return cleaned_words
 
-ttk.Label(frm, text="Enter Youtube Video ID").grid(column=0, row=1, sticky=W)
-ttk.Label(frm, text="Target Language").grid(column=0, row=2, sticky=W)
+    def compute_word_frequencies(self, words):
+        frequency = {}
+        for i in words:
+            if i == "":
+                continue
+            elif i in frequency:
+                frequency[i] += 1
+            else:
+                frequency[i] = 1
+        return frequency
 
-COMBOBOX = ttk.Combobox(frm, values=language_list, state='readonly')
+    def save_frequencies(self, frequency, filename):
+        sorted_frequency = sorted(frequency.items(), key=lambda x: x[1], reverse=True)
+        with open(f"{filename}.txt", "w", encoding="utf-8") as f:
+            for word, frequencies in sorted_frequency:
+                f.write(f"{word}: {str(frequencies)}" + "\n")
 
-COMBOBOX.grid(column=1, row=2, sticky=E)
+    def get_information(self):
+        lang_choice = self.combobox.get()
+        video_id = self.entryfield.get()
+        user_filename = self.filename.get()
+        locale.setlocale(locale.LC_ALL, self.locale_codes[lang_choice])
+        transcript = self.get_transcript(video_id, self.language_codes[lang_choice])
+        cleaned_words = self.clean_transcript(transcript)
+        frequency = self.compute_word_frequencies(cleaned_words)
+        self.save_frequencies(frequency, user_filename)
+        self.root.destroy()
 
-ENTRYFIELD = ttk.Entry(frm, width=10)
-ENTRYFIELD.grid(column=1, row=1, sticky=E)
-ttk.Label(frm, text="Enter Desired Filename").grid(column=0, row=4, sticky=W)
-FILENAME = ttk.Entry(frm, width=25)
-FILENAME.grid(column=1, row=4, sticky=E)
-ttk.Button(frm, text="Begin", command=get_information).grid(
-    column=0, row=5, sticky=W)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(
-    column=1, row=5, sticky=E)
-root.mainloop()
+    def main(self):
+        self.root.mainloop()
 
 
-print(COMBOBOX)
-print(ENTRYFIELD)
+if __name__ == "__main__":
+    app = Zwicky()
+    app.main()
+
