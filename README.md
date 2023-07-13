29683b7e97b90e9af759cc1552c87f78dbbbc6f3
uripper <kripper11@gmail.com>
2023-07-13 05:52:04 -0600

    The changes made in this commit include: - Added ignored_words.txt - Added sliders to ignore words based on their percentage usage. - Added checkboxes to use sliders or not. - Modified the get_information function to get the video ID from the URL. - Modified the compute_word_frequencies function to filter words based on the min and max frequency sliders. - Modified the save_frequencies function to save the frequency table into a file in a user-selected location and include the percentage of how often each word was used. - Modified the setup_gui function to include the new sliders and checkboxes.

--- README.md
+++ README.md
@@ -1,19 +1,21 @@
+
 # Zwicky
 
-Zwicky is an application that creates a frequency table from the most commonly used words in a YouTube video. It's named after Arnold Zwicky, a linguist who coined the term "recency illusion".
+Zwicky is an application that creates a frequency table from the most commonly used words in a YouTube video, and shows the percentage of how often each word was used. It's named after Arnold Zwicky, a linguist who coined the term "recency illusion".
 
 ## Features
 
-- Fetch YouTube video transcripts.
+- Fetch YouTube video transcripts from provided URL.
 - Process transcripts to clean and separate it into individual words.
-- Create a frequency table of the words from the cleaned transcript.
-- Save the frequency table into a file.
-- GUI for user input.
+- Create a frequency table of the words from the cleaned transcript, showing word usage percentages.
+- Sliders to ignore words based on their percentage usage.
+- Ignore words based on a list provided in an "ignored_words" file.
+- Save the frequency table into a file in a user-selected location.
 
 ## How to Use
 
 1. Start the application.
-2. In the GUI, enter the YouTube video ID in the corresponding field.
-3. Select the language of the transcript.
-4. Enter the desired filename for the output file.
-5. Click on "Begin" to start the process.
+2. In the GUI, enter the YouTube video URL in the corresponding field.
+3. Adjust the sliders to set the minimum and maximum word frequency for inclusion in the output.
+4. Click on "Begin" to start the process.
+5. Choose where to save the output file using the file browser that appears.
--- main.py
+++ main.py
@@ -1,48 +1,82 @@
-# Refactored Code
-
+import contextlib
 from youtube_transcript_api import YouTubeTranscriptApi
-from tkinter import *
-from tkinter import ttk
+from tkinter import ttk, filedialog, Tk, Scale, HORIZONTAL, W, E
 import locale
-import tomllib
+import toml
+import re
 
 
 class Zwicky:
     def __init__(self):
-        with open("config.toml", "rb") as f:
-            self.config = tomllib.load(f)
-
+        try:
+            with open("config.toml", "r") as f:
+                self.config = toml.load(f)
+        except FileNotFoundError:
+            error_message = "Config file not found. Please ensure that config.toml is in the same directory as this program. This can be downloaded from the Github repository."
+            raise FileNotFoundError(error_message)
+        try:
+            with open("ignored_words.txt", "r") as f:
+                self.ignored_words = f.read().splitlines()
+        except FileNotFoundError:
+            with open("ignored_words.txt", "w") as f:
+                f.write("")
+                self.ignored_words = []
         self.locale_codes = self.config["locale_codes"]
         self.language_codes = self.config["language_codes"]
 
         self.root = Tk()
-        self.root.title("Word Frequency Analyzer")
+        self.root.title("Zwicky: Word Frequency Analyzer")
+
         self.font_general = ("Roboto", 14)
+
         self.frm = ttk.Frame(self.root, padding=20)
         self.frm.grid()
+
         self.combobox = ttk.Combobox(
             self.frm, values=self.config["language_list"], state="readonly"
         )
-        self.entryfield = ttk.Entry(self.frm, width=10)
-        self.filename = ttk.Entry(self.frm, width=25)
+        self.entryfield = ttk.Entry(self.frm, width=25)
+
+        self.min_slider = Scale(self.frm, from_=0, to=5, orient=HORIZONTAL)
+        self.max_slider = Scale(self.frm, from_=3, to=10, orient=HORIZONTAL)
+        self.use_min_slider_box = ttk.Checkbutton(self.frm, text="Use Min Slider")
+        self.use_max_slider_box = ttk.Checkbutton(self.frm, text="Use Max Slider")
         self.setup_gui()
 
     def setup_gui(self):
-        ttk.Label(self.frm, text="Enter Youtube Video ID").grid(
+
+        ttk.Label(self.frm, text="Target Language").grid(column=0, row=0, sticky=W)
+        self.combobox.grid(column=1, row=0, sticky=E)
+
+        ttk.Label(self.frm, text="Enter Youtube Video URL").grid(
             column=0, row=1, sticky=W
         )
-        ttk.Label(self.frm, text="Target Language").grid(column=0, row=2, sticky=W)
-        self.combobox.grid(column=1, row=2, sticky=E)
         self.entryfield.grid(column=1, row=1, sticky=E)
-        ttk.Label(self.frm, text="Enter Desired Filename").grid(
+
+        ttk.Label(self.frm, text="Min Word Frequency (%)").grid(
+            column=0, row=2, sticky=W
+        )
+        self.min_slider.grid(column=1, row=2, sticky=E)
+        ttk.Label(self.frm, text="Use Min Slider").grid(column=2, row=2, sticky=W)
+        self.use_min_slider_box.grid(column=3, row=2, sticky=E)
+        self.min_slider.set(0)
+        self.use_min_slider_box.state(["selected"])
+
+        ttk.Label(self.frm, text="Max Word Frequency (%)").grid(
+            column=0, row=3, sticky=W
+        )
+        self.max_slider.grid(column=1, row=3, sticky=E)
+        self.max_slider.set(10)
+        ttk.Label(self.frm, text="Use Max Slider").grid(column=2, row=3, sticky=W)
+        self.use_max_slider_box.grid(column=3, row=3, sticky=E)
+        self.use_max_slider_box.state(["selected"])
+
+
+        ttk.Button(self.frm, text="Begin", command=self.get_information).grid(
             column=0, row=4, sticky=W
         )
-        self.filename.grid(column=1, row=4, sticky=E)
-        ttk.Button(self.frm, text="Begin", command=self.get_information).grid(
-            column=0, row=5, sticky=W
-        )
         ttk.Button(self.frm, text="Quit", command=self.root.destroy).grid(
-            column=1, row=5, sticky=E
+            column=1, row=4, sticky=E
         )
 
     def get_transcript(self, video_id, lang):
@@ -72,31 +106,51 @@ class Zwicky:
         return cleaned_words
 
     def compute_word_frequencies(self, words):
+        total_words = len(words)
         frequency = {}
         for i in words:
-            if i == "":
+            if i == "" or i in self.ignored_words:
                 continue
             elif i in frequency:
                 frequency[i] += 1
             else:
                 frequency[i] = 1
-        return frequency
 
-    def save_frequencies(self, frequency, filename):
+        # Filter words based on min and max frequencies
+        min_freq = self.min_slider.get() * total_words / 100
+        max_freq = self.max_slider.get() * total_words / 100
+
+        if not self.use_min_slider_box.instate(["selected"]):
+            min_freq = 0
+
+        if not self.use_max_slider_box.instate(["selected"]):
+            max_freq = total_words
+
+        filtered_frequency = {
+            word: count
+            for word, count in frequency.items()
+            if min_freq <= count <= max_freq
+        }
+        return filtered_frequency, total_words
+
+    def save_frequencies(self, frequency, total_words, filename):
         sorted_frequency = sorted(frequency.items(), key=lambda x: x[1], reverse=True)
-        with open(f"{filename}.txt", "w", encoding="utf-8") as f:
-            for word, frequencies in sorted_frequency:
-                f.write(f"{word}: {str(frequencies)}" + "\n")
+        with contextlib.suppress(FileNotFoundError):
+            with open(filename, "w", encoding="utf-8") as f:
+                for word, frequencies in sorted_frequency:
+                    percentage = (frequencies / total_words) * 100
+                    f.write(f"{word}: {str(frequencies)} ({percentage:.2f}%)" + "\n")
 
     def get_information(self):
+        url = self.entryfield.get()
+        video_id = re.findall(r"(?<=v=)[^&#]+", url)[0]
         lang_choice = self.combobox.get()
-        video_id = self.entryfield.get()
-        user_filename = self.filename.get()
         locale.setlocale(locale.LC_ALL, self.locale_codes[lang_choice])
         transcript = self.get_transcript(video_id, self.language_codes[lang_choice])
         cleaned_words = self.clean_transcript(transcript)
-        frequency = self.compute_word_frequencies(cleaned_words)
-        self.save_frequencies(frequency, user_filename)
+        frequency, total_words = self.compute_word_frequencies(cleaned_words)
+        filename = filedialog.asksaveasfilename(defaultextension=".txt")
+        self.save_frequencies(frequency, total_words, filename)
         self.root.destroy()
 
     def main(self):
@@ -106,4 +160,3 @@ class Zwicky:
 if __name__ == "__main__":
     app = Zwicky()
     app.main()
-
